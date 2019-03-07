027d7cb542b37e27075d7103f10fd976b013a16a
Paul Aliu <paul.iyola.aliu@gmail.com>
2019-03-07 11:44:47 +0000

    mg

--- wdi-fumdamentals-memorygame/memory_game/js/main.js
+++ wdi-fumdamentals-memorygame/memory_game/js/main.js
@@ -1,1 +1,10 @@
 console.log("Up and running!");
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
+
+var cards = [
+    "queen",
+    "queen",
+    "king",
+    "king"
+  ];
