aa23f7a0caa8908d4528236d3d97c51fc3698bd8
Paul Aliu <paul.iyola.aliu@gmail.com>
2019-03-07 22:00:05 +0000

    created game logic

--- wdi-fumdamentals-memorygame/memory_game/js/main.js
+++ wdi-fumdamentals-memorygame/memory_game/js/main.js
@@ -1,8 +1,29 @@
 var cards = [
-    "queen",
-    "queen",
-    "king",
-    "king"
+        {
+        rank: 'queen',
+        suit: 'hearts',
+        cardImage: "images/queen-of-hearts.png"
+        },
+        {
+        rank: 'queen',
+        suit: 'diamonds',
+        cardImage: "images/queen-of-diamonds.png"
+        },
+        {
+        rank: 'king',
+        suit: 'hearts',            
+        cardImage: "images/king-of-hearts.png"
+        },
+        {
+        rank: 'king',
+        suit: 'hearts',            
+        cardImage: "images/king-of-hearts.png" 
+        },
+        {
+        rank: 'king',
+        suit: 'diamonds',            
+        cardImage: "images/king-of-diamonds.png"             
+        }
 ]
 
 //Create an empty array called cardsInPlay
@@ -40,7 +61,9 @@ function flipCard(cardId)
 
        
     }*/
-    console.log("User flipped " + cards[cardId]) 
+    console.log("User flipped " + cards[cardId].rank) 
+    console.log(cards[cardId].cardImage)
+    console.log(cards[cardId].suit)
 }
 
 flipCard(0)
