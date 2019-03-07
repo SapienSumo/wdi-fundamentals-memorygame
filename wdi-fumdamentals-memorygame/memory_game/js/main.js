5b89121138e4ef941f1cf45c044b9bc54af77ace
Paul Aliu <paul.iyola.aliu@gmail.com>
2019-03-07 20:46:28 +0000

    created game logic

--- wdi-fumdamentals-memorygame/memory_game/js/main.js
+++ wdi-fumdamentals-memorygame/memory_game/js/main.js
@@ -43,21 +43,3 @@ if(lengthOfCardsInPlay ==2)
 
 
 console.log (cardsInPlay)
-
-
-/*console.log("Up and running!");
-
-var cards = [
-    "queen",
-    "king"
-]
-
-var cardOne = []
-
-//The first item of cards=>[0]
-cardOne.push(cards[0]);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
-//output the cardOne
-//console.log(cardOne)*/
